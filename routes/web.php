85c1074b1fb22ac9045eec5c80520f5ca5492d90
ruhulfbr <ruhul11bd@gmail.com>
2023-07-27 23:34:53 +0600

    Chat Application using laravel broadcast and pusher

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,15 @@
 <?php
 
+use App\Events\Message;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\App;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
-
-    $env = App::Environment();
-
-    if(App::Environment('local')){
-        echo 'Environment is local';
-    }
-    else{
-        echo 'Environment is '.$env;
-    }
-
-    exit();
-
     return view('welcome');
+});
+
+Route::post('send-message',function (Request $request){
+    event(new Message($request->username, $request->message));
+    return ['success' => true];
 });
