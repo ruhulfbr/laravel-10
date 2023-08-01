fbd02a438f9bbd112a1922620926ff17bb12b3e0
ruhulfbr <ruhul11bd@gmail.com>
2023-08-01 06:26:13 +0600

    Job  middleware

---
+++ app/Jobs/Middleware/RateLimited.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Redis;
+
+class RateLimited
+{
+    /**
+     * Process the queued job.
+     *
+     * @param  \Closure(object): void  $next
+     */
+    public function handle(object $job, Closure $next): void
+    {
+        Redis::throttle('key')
+            ->block(0)->allow(1)->every(5)
+            ->then(function () use ($job, $next) {
+                // Lock obtained...
+
+                $next($job);
+            }, function () use ($job) {
+                // Could not obtain lock...
+
+                $job->release(5);
+            });
+    }
+}
---
+++ app/Jobs/ProcessPodcast.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Redis;
+use App\Jobs\Middleware\RateLimited;
+
+
+class ProcessPodcast implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Redis::throttle('key')->block(0)->allow(1)->every(5)->then(function () {
+            info('Lock obtained...');
+
+            // Handle job...
+        }, function () {
+            // Could not obtain lock...
+
+            return $this->release(5);
+        });
+    }
+
+    public function middleware(): array
+    {
+        return [new RateLimited];
+    }
+}
